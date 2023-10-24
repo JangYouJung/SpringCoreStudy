e357a569d789e51047898d97510abed8e0523a5c
You Jeong Jang <sunny819819@naver.com>
2023-10-24 23:21:06 +0900

    3. 회원 도메인

---
+++ core/src/main/java/hello/core/member/Member.java
@@ -0,0 +1,38 @@
+package hello.core.member;
+
+public class Member {
+    public Member(Long id, String name, Grade grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Grade grade) {
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    private Long id;
+    private String name;
+    private Grade grade;
+}
