95b635b86e9166e2701e9dda429b9208b71a040c
You Jeong Jang <sunny819819@naver.com>
2023-11-12 23:59:36 +0900

    Update OrderServiceImpl.java
    DIP OCP 위반

--- core/src/main/java/hello/core/Order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/Order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.Order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -10,7 +11,7 @@ public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
     private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-
+    //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
