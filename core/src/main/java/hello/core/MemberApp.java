58ec498e04ff55d9de7f2e04d01332d4b3117db1
You Jeong Jang <sunny819819@naver.com>
2023-11-14 14:16:21 +0900

    Update MemberApp.java

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -9,12 +9,14 @@ import hello.core.member.MemberServiceImpl;
 //Spring과 관련 없는 순수한 자바로 개발한 코드 : 한계가 있음, 항상 눈으로 직접 확인해야
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberServiec();
+ //     MemberService memberService = new MemberServiceImpl();
         Member member = new Member(1L,"memberA", Grade.VIP);
         memberService.join(member);
 
         Member findMember = memberService.findMember(1L);
-        System.out.println("new member = " + member);
+        System.out.println("new member = " + member.getName());
         System.out.println("findMember = "+findMember.getName());
 
     }
