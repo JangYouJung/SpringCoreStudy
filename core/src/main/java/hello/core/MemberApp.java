10e8748e93117008c096e5f961cad0c538e8bd9e
You Jeong Jang <sunny819819@naver.com>
2023-11-15 16:24:35 +0900

    Update MemberApp.java

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -4,14 +4,19 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
 //Spring과 관련 없는 순수한 자바로 개발한 코드 : 한계가 있음, 항상 눈으로 직접 확인해야
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
- //     MemberService memberService = new MemberServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);//애노테이션을 기반으로 cinfig
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class); //이름과 타입
+
         Member member = new Member(1L,"memberA", Grade.VIP);
         memberService.join(member);
 
