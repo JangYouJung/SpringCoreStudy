460e4d66de7a0ed2016f05aefbaaafa37edd2939
You Jeong Jang <sunny819819@naver.com>
2023-11-28 19:42:25 +0900

    Update AppConfig.java

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -18,16 +18,19 @@ public class AppConfig {
 
     @Bean
     public static MemoryMemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public MemberService memberService(){
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
