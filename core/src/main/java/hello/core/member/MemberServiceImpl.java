52889b4838077b4b88a9c9d456ea7e8529d204a9
You Jeong Jang <sunny819819@naver.com>
2023-11-29 16:56:06 +0900

    Update MemberServiceImpl.java

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -15,4 +15,10 @@ public class MemberServiceImpl implements MemberService{
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
     }
+
+
+    //테스트용
+    public MemberRepository getMemberRepository(){
+        return memberRepository;
+    }
 }
