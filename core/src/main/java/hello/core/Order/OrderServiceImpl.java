78d123dd90ab66dbff8f8c871bf7e8674dc0612d
You Jeong Jang <sunny819819@naver.com>
2023-12-27 17:04:55 +0900

    롬복으로 생성자 자동 생성

--- core/src/main/java/hello/core/Order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/Order/OrderServiceImpl.java
@@ -6,14 +6,27 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
+@Getter
 public class OrderServiceImpl implements OrderService{
 
+    //테스트용
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+    
+    /* 롬복으로 생성자 자동 생성
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy){
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+    */
 
     /* 수정자 주입 에러 발생
     @Autowired
@@ -26,14 +39,6 @@ public class OrderServiceImpl implements OrderService{
     }
     */
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy){
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
-
-
-
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
@@ -42,8 +47,4 @@ public class OrderServiceImpl implements OrderService{
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
 
-    //테스트용
-    public MemberRepository getMemberRepository(){
-        return memberRepository;
-    }
 }
