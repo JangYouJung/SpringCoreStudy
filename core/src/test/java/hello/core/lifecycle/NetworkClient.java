f0726a5071309f422df325a9ebbe21f17de1234a
You Jeong Jang <sunny819819@naver.com>
2024-01-02 15:34:21 +0900

    세션8: [2] 설정정보자유사용

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,7 +3,7 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
     private String url;
 
     public NetworkClient(){
@@ -30,16 +30,15 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: "+ url);
     }
 
-
-    // 인터페이스 추가
-    @Override
-    public void destroy() throws Exception {
-        disConnect();
-    }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
+
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disConnect();
+    }
+
 }
