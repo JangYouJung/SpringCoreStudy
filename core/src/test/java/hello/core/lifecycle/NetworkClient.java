c7743b07cbf85a1ddb8d3e58612e0e8937bd8d58
You Jeong Jang <sunny819819@naver.com>
2024-01-02 15:28:16 +0900

    세션8: [1]스프링 전용 인터페이스에 의존

---
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,45 @@
+package hello.core.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
+    private String url;
+
+    public NetworkClient(){
+        System.out.println("생성자 호출, url = " + url);
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // 서비스 시작시 호출
+    public void connect(){
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String message){
+        System.out.println("call: " + url + " message = " + message);
+    }
+
+    //서비스 종료시 호출
+    public void disConnect(){
+        System.out.println("close: "+ url);
+    }
+
+
+    // 인터페이스 추가
+    @Override
+    public void destroy() throws Exception {
+        disConnect();
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메시지");
+    }
+}
