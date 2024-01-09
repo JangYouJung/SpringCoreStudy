37601dc90db5b714892935d6478189659cad860b
You Jeong Jang <sunny819819@naver.com>
2024-01-09 13:42:41 +0900

    9-2: ObjectProvider 사용

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,6 +1,7 @@
 package hello.core.scope;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.cassandra.DataCassandraTest;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -37,12 +38,23 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
 
     }
 
     @Scope("singleton")
     static class ClientBean {
+        @Autowired
+        private ObjectProvider<PrototypeBean> prototypeBeansProvider;
+
+        public int logic(){
+            PrototypeBean prototypeBean = prototypeBeansProvider.getObject();
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
+
+        /*
         private final PrototypeBean prototypeBean; //생성 시점에 주입
         @Autowired
         public ClientBean(PrototypeBean prototypeBean) { this.prototypeBean = prototypeBean;
@@ -52,6 +64,8 @@ public class SingletonWithPrototypeTest1 {
             int count = prototypeBean.getCount();
             return count;
         }
+        *
+         */
     }
 
 
